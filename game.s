36255834896bf9d44b402c19351e403b847f60d7
M. Cardoso <cardosaum@pm.me>
2021-04-28 10:33:45 -0300

    add function and macro to print a sprite

--- game.s
+++ game.s
@@ -1,30 +1,15 @@
 .data
     .include "lolo.data"
+    .include "lolo_front.data"
     .include "map.data"
     .include "macros.s"
 
 .text
 MAIN:
     init()
-    #print_pixel(0, 0, 100)
-    #print_pixel(100, 0, 100)
-    #print_pixel(100, 100, 100)
-    #print_pixel(101, 101, 100)
-    #print_pixel(102, 102, 100)
-    #print_pixel(320, 240, 100)
-    #li a0,100
-    #li a1,200
-    #li a2,255
-    #jal PRINT_PIXEL
-    #li a1,220
-    #jal PRINT_PIXEL
-    #li a1,180
-    #li a2,7
-    #jal PRINT_PIXEL
-    li a0,40
-    li a1,40
-    la a3,lolo
-    jal PRINT_SPRITE
+    print_sprite(0, 0, map)
+    print_sprite(100, 100, lolo)
+    print_sprite(200, 200, lolo_front)
     exit()
 
 PRINT_PIXEL:
@@ -39,33 +24,40 @@ PRINT_PIXEL:
     ret
 
 PRINT_SPRITE:
-    # a0: x
-    # a1: y
+    # a0: x_base_pixel
+    # a1: y_base_pixel
     # a3: sprite address
     lw t1,0(a3) # t1 = image_width
     lw t2,4(a3) # t2 = image_height
     mul t3,t1,t2 # t3 = t1*t2 (image area)
     srli t3,t3,2 # t3 /= 4
-    li t4,0 # actual_column
     addi t5,a3,8 # t5 = first 4 pixels
     mv t0,s0 # frame address copy
+    add t0,t0,a0 # frame_address + x_base_pixel
+    mul t4,s1,a1 # t4 = canvas_width * y_base_pixel
+    add t0,t0,t4 # frame_address += t4
+    li t4,0 # (t4) actual_column = 0
 
 PRINT_SPRITE_LOOP:
     lw t6,(t5) # load pixel from HD
     sw t6,(t0) # print pixel to frame
-    addi t4,t4,1
-    addi t5,t5,4 # pixel_address += 4
-    addi t0,t0,4 # frame_address += 4
+    addi t4,t4,4 # actual_column += 4
     addi t3,t3,-1 # image_area -= 1
-    bge t4,t1,PRINT_SPRITE_NEXT_LINE # if a0 >= t1: goto PRINT_SPRITE_NEXT_LINE
+    addi t5,t5,4 # pixel_address += 4
     beqz t3,PRINT_SPRITE_LOOP_EXIT # if thereis no more pixel to print, exit.
+    bge t4,t1,PRINT_SPRITE_NEXT_LINE # if a0 >= t1: goto PRINT_SPRITE_NEXT_LINE
+    addi t0,t0,4 # frame_address += 4
     addi a0,a0,1 # x += 1
     j PRINT_SPRITE_LOOP
 
 PRINT_SPRITE_NEXT_LINE:
-    li a0,0
-    li t4,0
-    addi a1,a1,1
+    addi a1,a1,1 # y_base_pixel += 1
+    add t0,t0,s1 # frame_address += canvas_width
+    mv t4,t1 # t4 = t1
+    addi t4,t4,-4 # t4 -= 4 (we ignore the last printed address)
+    sub t0,t0,t4 # t0 -= t4
+    li a0,0 # x_base_pixel = 0
+    li t4,0 # actual_column = 0
     j PRINT_SPRITE_LOOP
 
 PRINT_SPRITE_LOOP_EXIT:
--- macros.s
+++ macros.s
@@ -1,39 +1,3 @@
-#.macro render(%img_address,%coluna,%linha,%frame)
-    #li t1 0xFF0        # t1 = 0xFF0
-    #add t1 t1 %frame    # t1 = 0xFF0 + frame
-    #slli t1 t1 20        # seta t1 para o formato do endereço do bitmap display
-#
-    #mv t0 %img_address    # carrega em t0 endereco imagem
-#
-    #li t2 256        # t2 = 256 total de pixels
-    #li t3 0            # t3 = 0 contador imagem
-    #li t6 0            # t6 = 0 contador coluna
-    #li t5 320        # t5 = 320
-    #mul t5 %linha t5    # multiplica linha por 320 (põe linha correspondente ao endereco bitmap)
-    #add t5 t5 %coluna    # t5 = linha setada + coluna
-    #add t1 t1 t5        # soma t5 ao endereco display
-    #addi t0 t0 8        # soma 8 a endereco imagem
-    #li t5 16        # t5 = 16 pixels na coluna
-#
-#LOOP:
-    #beq t3 t2 EXIT2        # if t3 = t2 go to EXIT2
-    #lw t4 0(t0)        # carrega em t4 4 pixels
-    #sw t4 0(t1)        # bota na tela 4 pixels
-    #addi t0 t0 4
-    #addi t1 t1 4        # t0, t1, t3 e t4 sao acrescidos de 4
-    #addi t3 t3 4
-    #addi t6 t6 4
-    #beq t6 t5 EXIT1        # if t4 = t5 go to EXIT1
-    #j LOOP
-#
-#EXIT1:
-    #mv t6 zero        # t6 = 0
-    #addi t1 t1 0x130    # soma endereco display + 304
-    #j LOOP
-#
-#EXIT2:
-#.end_macro
-
 .macro init()
     # s0: frame selected
     # s1: canvas width
@@ -56,4 +20,11 @@
     add t5,s0,t1
     li t4,%color
     sw t4,(t5)
+.end_macro
+
+.macro print_sprite(%x, %y, %sprite_address)
+    li a0,%x
+    li a1,%y
+    la a3,%sprite_address
+    jal PRINT_SPRITE
 .end_macro
