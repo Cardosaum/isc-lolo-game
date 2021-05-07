29331c6e90d67d7f7c562ad2a472d48ac5ae9fb4
M. Cardoso <cardosaum@pm.me>
2021-05-07 15:39:38 -0300

    update

--- src/read_map_matrix.s
+++ src/read_map_matrix.s
@@ -1,4 +1,12 @@
 #====================================================================================================
-READ_AND_PRINT_MAP_MATRIX_DYNAMIC_SPRITES
-read_and_print_map_matrix_dynamic_sprites
+READ_AND_PRINT_MAP_MATRIX_DYNAMIC_SPRITES:
+    # this function parses the map represented by an array in 'a0' and print all needed dynamic sprites
+
+    # a0: map_matrix_array // something similar to MAP_1_MATRIX
+    # a1: size of map_matrix_array // something similar to 300 (for MAP_1_MATRIX for instance)
+
+READ_AND_PRINT_MAP_MATRIX_DYNAMIC_SPRITES_LOOP:
+    blez a1,READ_AND_PRINT_MAP_MATRIX_DYNAMIC_SPRITES_EXIT
+
+READ_AND_PRINT_MAP_MATRIX_DYNAMIC_SPRITES_EXIT:
 #====================================================================================================
