bf27ab13ddf2349aa03982bb5aa345e0868dfbd4
M. Cardoso <cardosaum@pm.me>
2021-05-07 14:40:51 -0300

    update

--- quick_and_dirty/dynamic_sprites.py
+++ quick_and_dirty/dynamic_sprites.py
@@ -5,8 +5,33 @@ ds = []
 lolo = {
     'current_position': (40, 40),
     'next_position': (120, 80),
-    'sprite_image': range(200)
+    'sprite_image': range(200),
+    'collision': True
+}
+
+heart1 = {
+    'current_position': (40, 40),
+    'next_position': (120, 80),
+    'sprite_image': range(200),
+    'collision': True
+}
+
+heart2 = {
+    'current_position': (40, 40),
+    'next_position': (120, 80),
+    'sprite_image': range(200),
+    'collision': True
+}
+
+snake = {
+    'current_position': (40, 40),
+    'next_position': (120, 80),
+    'sprite_image': range(200),
+    'collision': True
 }
 
 ds.append(lolo)
+ds.append(heart1)
+ds.append(heart2)
+ds.append(snake)
 pprint(ds)
