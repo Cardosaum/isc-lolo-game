b38393861da4ed87d7a29df9a02921ddd7645c00
Eduardo Ferreira <eduardo.ferreira.mc.edu@gmail.com>
2021-05-19 11:18:48 -0300

    alguns procs

---
+++ src/proc_check_map_hearts.s
@@ -0,0 +1,7 @@
+#====================================================================================================
+#procedimento checar corações fase 1 e dar o tiro
+# a0 = ID do mapa
+
+CHECK_NUMBER_OF_HEARTS:
+    la a0,MAP_LOLO_ID
+    
